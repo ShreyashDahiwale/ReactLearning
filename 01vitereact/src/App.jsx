907c1872cc1971be32fe804c667f1e8806d1bbc5
ShreyashDahiwale <shreyash.wale002@gmail.com>
2024-08-28 01:21:43 +0530

    Some notes added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,28 @@
+import Chai from "./chai"
+
+// function App() {
+
+//   return (
+//     <h1>Chai Aur React with Vite</h1>
+//   )
+// }
+
+
+// function App() {
+//   // <></> is called as Fragment in React
+//   return (
+//     <>  
+//       <Chai/>
+//     </>
+//   )
+// }
 
 function App() {
 
   return (
-    <h1>Chai Aur React with Vite</h1>
+    <div>  
+      <Chai/>
+    </div>
   )
 }
-
 export default App
