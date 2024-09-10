3211ba15e2a8d6d0bfdbdc2c5e546657951656c3
ShreyashDahiwale <shreyash.wale002@gmail.com>
2024-09-11 00:22:47 +0530

    Nested Routing

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -32,7 +32,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />}/>
-      <Route path="about" element={<About />}/>
+      <Route path="about" element={<About />}>
+        <Route path='shreyash' />
+      </Route>
       <Route path="contact" element={<Contact />}/>
       <Route path="user/:userid" element={<User />}/>
       <Route 
